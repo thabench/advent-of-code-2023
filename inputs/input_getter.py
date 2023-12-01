2d1cc828866df9d03361edbbc09d53c8c40739e5
thabench <92112588+thabench@users.noreply.github.com>
2023-12-01 17:13:12 +0200

    Add requirements.txt

--- inputs/input_getter.py
+++ inputs/input_getter.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
 cookies = {
     '_ga': 'GA1.2.262409860.1699862836',
     '_gid': 'GA1.2.1182568215.1699862836',
-    'session': '53616c7465645f5f0bc8fb771db0e997b50787bd9367a52032f82e5c5ad960a4b7e1abedfc3dd00c26dbce8b4996c590e4e5800b068cad5e1c1e1883eddb85b8',
+    'session': f'{os.environ["AOC_SESSION_ID"]}',
     '_gat': '1',
     '_ga_MHSNPJKWC7': 'GS1.2.1699894081.2.1.1699898276.0.0.0',
 }
@@ -12,7 +13,6 @@ headers = {
     'authority': 'adventofcode.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    # 'cookie': '_ga=GA1.2.262409860.1699862836; _gid=GA1.2.1182568215.1699862836; session=53616c7465645f5f0bc8fb771db0e997b50787bd9367a52032f82e5c5ad960a4b7e1abedfc3dd00c26dbce8b4996c590e4e5800b068cad5e1c1e1883eddb85b8; _gat=1; _ga_MHSNPJKWC7=GS1.2.1699894081.2.1.1699898276.0.0.0',
     'referer': 'https://adventofcode.com/2023/events',
     'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
